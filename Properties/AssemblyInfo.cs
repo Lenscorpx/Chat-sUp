1d64c39d0cc619c20e9eb00f75e0afa534cabf40
Lens Mutombo <lensmutombo@gmail.com>
2019-12-19 07:59:13 +0200

    Fini app

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ChatsUp")]
+[assembly: AssemblyDescription("Chat Application")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("LensCorp Enterprises Corporation")]
+[assembly: AssemblyProduct("ChatsUp")]
+[assembly: AssemblyCopyright("Copyright LensCorp Enterprises ©  2015")]
+[assembly: AssemblyTrademark("LensX")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(true)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3ff2bb77-2dd3-4b7d-afb2-ea4e4aaf863a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
